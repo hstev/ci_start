b2cac7c07bd54ce0992981ac83cb2f1f70108e6c
Harold Restrepo <hstevdev@gmail.com>
2020-08-27 21:51:57 -0500

    Ready to use with Bootstrap 4

---
+++ application/views/templates/footer.php
@@ -0,0 +1,28 @@
+    <footer class="pt-5">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12 my-auto mx-auto text-muted text-center">
+            <p>&copy; <?= date("Y"); ?>. All rights reserved</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="node_modules/@fortawesome/fontawesome-free/js/all.min.js"></script>
+    <script src="node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
+    <script src="node_modules/jquery-confirm/dist/jquery-confirm.min.js"></script>
+    <script src="node_modules/glightbox/dist/js/glightbox.min.js"></script>
+    <script src="assets/js/main/main.js"></script>
+    <?php
+    //load js
+    if (isset($libs) && count($libs) > 0) 
+    {
+      foreach ($libs as $lib)
+      {
+        echo '<script src="' . $lib . '?no_cache=' . rand(0, 10) . '"></script>';
+      }
+    }    
+    ?>
+  </body>
+</html>
---
+++ application/views/templates/head.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <base href="<?= base_url(); ?>">
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
+
+    <!--Social media-->
+    <meta property="og:title" content="">
+    <meta property="og:description" content="">
+    <meta property="og:image" content="">
+    <meta property="og:url" content="">
+    <meta name="twitter:title" content="">
+    <meta name="twitter:description" content="">
+    <meta name="twitter:image" content="">
+    <meta name="twitter:card" content="">
+
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.min.css"/>
+    <link rel="stylesheet" href="node_modules/jquery-confirm/dist/jquery-confirm.min.css">
+    <link rel="stylesheet" href="node_modules/animate.css/animate.min.css"/>
+    <link rel="stylesheet" href="node_modules/glightbox/dist/css/glightbox.css">
+    <link rel="stylesheet" href="assets/css/main/main.css?no_cache=<?= rand(1,10); ?>">
+    <?php
+      if(isset($styles) && count($styles) > 0)
+      {
+        foreach ($styles as $style)
+        {
+          echo '<link rel="stylesheet" type="text/css" href="'.$style.'?no_cache='.rand(1,10).'">';
+        }
+      }
+    ?>
+    <link rel="shortcut icon" href="assets/img/main/favicon.png">
+    <title>App <?= (isset($tab_name))? "| ".$tab_name : ""; ?></title>
+  </head>
+  <body>
